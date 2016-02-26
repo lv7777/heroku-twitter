004ca1aa46e65bc529b0487126f20e4512bbcaf9
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-02-27 01:16:28 +0900

    ji

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
- 
-//ルーティング設定
-app.get('/', function (req, res) {
-  res.send('Hello World');
+
+app.set('port', (process.env.PORT || 5000));
+
+// app.use(express.static(__dirname));
+
+// // views is directory for all template files
+ app.set('views', __dirname);
+// app.set('view engine', 'ejs');
+
+app.set('view engine', 'html');
+
+
+app.get('/', function(request, response) {
+  response.render('index');
 });
- 
-//以下、ルーティング情報があればここに追記していく
- 
-app.listen(80);
-console.log('Server running at http://localhost:3000/');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
