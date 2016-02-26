f5f3255ba0ffdf7d0366d64270907ee1209e29af
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-02-27 01:54:41 +0900

    update

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+var passport = require('passport');
+// モジュールロード
+// passport-twitterモジュールを事前にnpm installしておく必要があります。
+var passport = require('passport')
+  , TwitterStrategy = require('passport-twitter').Strategy;
+  
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+ 
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new TwitterStrategy({
+    // 上記アプリ登録で取得したキーを指定します。
+    consumerKey: "U5eGtwDWMXlaJbpJwcPiT3JZI",
+    consumerSecret: "zMNYm243jVxMn9VavwyoTo3TNfTYKrli8TuGJQ59dxVtAYPTiu",
+    // callbackで受け取りたいURLを指定します。
+    callbackURL: "https://twitter-auth-test.herokuapp.com/callback"
+  },
+  // Callback時の処理を記載します。
+    function (token, tokenSecret, profile, done) {
+        console.log(token, tokenSecret, profile);
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
+));
+
+
 var express = require('express');
 var app = express();
 
@@ -16,6 +47,15 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
+// 認証を開始する処理を記載します。
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+// コールバックで呼び出された際の処理を記載します。
+app.get('/callback', 
+  passport.authenticate('twitter', { successRedirect: '/',
+                                     failureRedirect: '/login' }));
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
