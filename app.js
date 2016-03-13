7b15a181d1f18dd41830a89b34eb6bc1fe9ee234
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-03-13 11:25:30 +0900

    global_gitignoreをgit_ignoreでオーバーライド
    passwardフォルダの中身だけを除外する。
    .gitignoreとpassworddir,pass.jsを作成

--- app.js
+++ app.js
@@ -5,29 +5,32 @@ var passport = require('passport')
   , TwitterStrategy = require('passport-twitter').Strategy;
 var session = require('express-session'); 
   
+  
 var express = require('express');
 var app = express();
 app.use(session({ secret: 'SECRET' }));
   app.use(passport.initialize());
   app.use(passport.session()); // persistent login sessions
 app.set('port', (process.env.PORT || 5000));
- app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
+ 
+ var ejs = require("ejs");
 // // views is directory for all template files
  app.set('views', __dirname);
-// app.set('view engine', 'ejs');
-
-app.set('view engine', 'html');
+ app.engine('ejs', ejs.renderFile); 
+ app.set('view engine', 'ejs');
 
 //一度認証した後はクッキーを利用することでセッションを確立・維持する作りになっています
 passport.serializeUser(function (user, done) {
     console.log("シリアライズ？");
     done(null, user);
 });
- 
+
 passport.deserializeUser(function (obj, done) {
     console.log("デシリアライズ？");
     done(null, obj);
 });
+
 //Passportは認証のためにストラテジーと呼ばれるものを認証に使用します。
 passport.use(new TwitterStrategy({
     // 上記アプリ登録で取得したキーを指定します。
@@ -36,7 +39,7 @@ passport.use(new TwitterStrategy({
     // callbackで受け取りたいURLを指定します。
     callbackURL: "http://127.0.0.1:5000/callback"
   },
-  // Callback時の処理を記載します。
+  // 検証用コールバック。検証するだけ
     function (token, tokenSecret, profile, done) {
         console.log("callback時の処理を記述します？");
  //       console.log(token, tokenSecret, profile);
@@ -54,13 +57,14 @@ app.get('/', function(request, response) {
  
 // /oauthにアクセスした時
 app.get('/auth/twitter',passport.authenticate('twitter'), function (req, res, next) {
+    console.log("hello");
     console.log(req, res, next);
 });
 
 // /oauth/callbackにアクセスした時（Twitterログイン後）
 app.get('/callback',passport.authenticate('twitter', { successRedirect: '/success',failureRedirect: '/fail' }), function(req, res) {
     console.log("ok!");
-  // res.redirect('/success'); //indexへリダイレクトさせる
+   res.redirect('/success'); //indexへリダイレクトさせる
 });
 
 //app.use('/success', express.static(__dirname));
@@ -68,8 +72,22 @@ app.get('/callback',passport.authenticate('twitter', { successRedirect: '/succes
 app.get('/success',function(req,res){
     console.log("認証成功！");
     console.log(req.user);
-//    res.render('s1');
+    var data=req.user;//JSON.parse(req.user);
+    console.log(data.name);
+    console.log(req.user.name)
+ /*   for(var a in req.user){
+        console.log(a+"   a");
+      console.log(req.user[a])  
+    }*/
+    var rendobj={
+        name:data.displayName,
+        img:data._json.profile_image_url,
+        tweet:data._json.description
+    };
+    res.render('success.ejs',rendobj);
 });
+
+
 app.get('/fail',function(req,res){
     console.log("認証失敗・・・");
  //   console.log(req.user);
